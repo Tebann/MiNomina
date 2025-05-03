ac7f6477d7346c6fe6bcfe4bc5046b6e78812028
jeronimo <jeronimor.dev@gmail.com>
2025-05-03 21:43:25 +0000

    actualizaciones de back y login sql

--- ProyectoMiNomina/js/api.js
+++ ProyectoMiNomina/js/api.js
@@ -1,5 +1,5 @@
 // API Service para conectar con el backend
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'http://localhost:3000/api';
 
 // Almacenamiento del token
 let authToken = localStorage.getItem('token') || null;
